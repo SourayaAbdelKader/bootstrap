22e970afd066ec9ddf7c45227770491c88c3e04f
Sourayaak <sourayaabdelkader.gmail.com>
2022-09-12 22:00:40 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function validateFullName() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+
+function validateEmail() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+
+function validatePhoneNumber() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+}
+
+function validateMessage() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+}

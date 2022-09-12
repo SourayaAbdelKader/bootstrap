04c4945fa863a6a139c80a1102de95bb9e2600b0
Sourayaak <sourayaabdelkader.gmail.com>
2022-09-12 22:21:30 +0300

    JS: insert function to validate name and email

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
+//Full Names should be a minimum of 5 characters
 function validateFullName() {
-    let x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-      alert("Name must be filled out");
+    let full_name = document.getElementById("full_name").value;
+    if (full_name.length < 5) {
       return false;
-    }
-  }
+    };
+};
 
+//Email field should be an email, with a minimum of 3 characters before the @ and 5 characters after the @. 
 function validateEmail() {
-    let x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-  }
+    let email = document.getElementById("email").value;
+    let email_array = text.split("@");
+    if (email_array[0].length < 3 || email_array[1].length <5) {
+        return false;
+    }; 
+};
 
+//Phone Number should start with +961 and followed by either 7 digits (if it's a 03 number, i.e: +9613456789) or. followed by 8 digits (if it's a 71/76/70/etc: +96171345234).
 function validatePhoneNumber() {
-    let x = document.forms["myForm"]["fname"].value;
+    let phone_numer = document.getElementById("phone_number").value;
     if (x == "") {
-      alert("Name must be filled out");
       return false;
-    }
+    };
+
 }
 
+//Message should be a minimum of 100 characters.
 function validateMessage() {
-    let x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-      alert("Name must be filled out");
+    let message = document.getElementById("message").value;
+    if (message.length < 100) {
       return false;
-    }
-}
+    };
+};
+
+//If the validation fails, please show a div (section) under the Contact Me title highlighting what went wrong with a red background. 

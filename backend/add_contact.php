47fe96681663f9e7551faae51a2f11c10d34e53e
Sourayaak <sourayaabdelkader.gmail.com>
2022-09-14 09:53:25 +0300

    php: add contacts

---
+++ backend/add_contact.php
@@ -0,0 +1,19 @@
+<?php
+
+/
+include("connection.php");
+
+$name = $_POST["full-name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$message = $_POST["message"]
+
+$query = $mysqli->prepare("INSERT INTO users(name, email, phone, message) VALUE (?, ?, ?, ?)");
+$query->bind_param("ssss", $name, $email, $phone, $message);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+?>
